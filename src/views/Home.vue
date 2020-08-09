b96814ca434db920d9d2adc05b2aa77c43e386c1
Parada <paradafaii28@gmail.com>
2020-08-08 18:49:05 -0600

    add content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="home">
-    <img class="img-header" alt="leaf logo" src="../assets/leaf2.jpg">
+  <div class="home-container">
+    <div class="header-container">
+      <h1>HOW TO GO GREEN</h1>
+      <p class="paragrah-container"> A lot of people are daunted by the idea of “going green” as it may seem like one would need to make sweeping lifestyle changes to be able to make a difference. Fortunately, that’s not the case.</p>
+    </div>
+    <div class="image-container"> 
+      <img class="img-header" alt="leaf logo" src="../assets/leaf2.jpg">
+    </div>
   </div>
 </template>
 
@@ -16,13 +22,18 @@
 </script>
 
 <style scoped>
-  .home {
-    width: 100%;
+  .home-container {
+    display: flex;
+    flex-direction: row;
+    margin-left: 10em;
+  }
+  .header-container {
+    width: 25%;
+    margin-top: 10em;
   }
   .img-header {
-    margin-left: 44em;
+    margin-left: 30em;
     margin-top: 5em;
     width: 57%;
-    
   }
 </style>
