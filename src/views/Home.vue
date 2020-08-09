77d8e64abfea75f3b029620646a19ce014f5804e
Parada <paradafaii28@gmail.com>
2020-08-08 21:02:45 -0600

    import the component to home file

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,20 @@
 <template>
   <div class="home-container">
-    <div class="header-container">
+    <div class="first-container">
       <h1 class="header">HOW TO GO GREEN</h1>
       <p class="paragrah-container"> A lot of people are daunted by the idea of <strong>“going green”</strong> as it may seem like one would need to make sweeping lifestyle changes to be able to make a difference. Fortunately, that’s not the case.</p>
     </div>
     <div class="image-container"> 
       <img class="img-header" alt="leaf logo" src="../assets/leaf2.jpg">
     </div>
-  </div>
+  </div>  
 </template>
 
 <script>
-// @ is an alias to /src
-
+// import SecondHome from './SecondHome.vue'
 // export default {
-//   name: 'Home',
 //   components: {
-    
+//       SecondHome
 //   }
 // }
 </script>
@@ -27,7 +25,7 @@
     flex-direction: row;
     margin-left: 10em;
   }
-  .header-container {
+  .first-container {
     width: 25%;
     margin-top: 10em;
   }
---
+++ src/views/SecondHome.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="second-container">
+      <h1>Want To Be Happier, Healthier, And More Productive? Go Green.</h1>
+      <p></p>
+    </div>     
+</template>
