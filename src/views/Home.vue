055188aa9f8607950648f9746c06d28ebd434085
Parada <paradafaii28@gmail.com>
2020-08-15 14:40:15 -0600

    add the content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
-  <section class="home-container">  
+  <div class="home-container">  
       <div class="first-container">
         <h1 class="header">HOW TO GO GREEN</h1>
         <p class="paragrah-container"> A lot of people are daunted by the idea of <strong>“going green”</strong> as it may seem like one would need to make sweeping lifestyle changes to be able to make a difference. Fortunately, that’s not the case.</p>
+        <p class='content'>Want To Be Happier, Healthier, And More Productive? Go Green.</p>  
       </div>
       <div class="image-container"> 
-        <img class="img-header" alt="leaf logo" src="../assets/leaf2.jpg">
+        <img class="img-header" alt="leaf-header" src="../assets/leaf2.jpg">
       </div>
-      <separator></separator>
-      <div class="secondparagrah-container">
-        <p class='content'>Want To Be Happier, Healthier, And More Productive? Go Green.</p>
+      <div>
       </div>
-  </section>  
+  </div>  
 </template>
 
 <script>
@@ -43,15 +42,17 @@
   .img-header {
     margin-left: 30em;
     margin-top: 5em;
-    width: 75%;
+    width: 60%;
   }
   .image-container {  
     height: 40%;
- 
   }
-  .secondparagrah-container {
-    margin-top: 50%;
+  .content {
+    width: 70%;
+    margin-top: 30em;
+    margin-left: 30em;
   }
 </style>
+
 
 
