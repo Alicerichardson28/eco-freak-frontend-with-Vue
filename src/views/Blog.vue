c7e0e0b4925d3afeab96cc50c57968d0f16284a8
Parada <paradafaii28@gmail.com>
2020-08-26 14:43:05 -0600

    add blog pages

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,12 +1,10 @@
 <template>
 <div class="jumbotron text-center hoverable p-4">
-
   <div class="row">
-
     <div class="col-md-4 offset-md-1 mx-3 my-3">
 
       <div class="view overlay">
-        <img src="https://mdbootstrap.com/img/Photos/Others/laptop-sm.jpg" class="img-fluid" alt="Sample image for first version of blog listing">
+        <img src="../assets/eco-friendly.jpg" class="img-fluid" >
         <a>
           <div class="mask rgba-white-slight"></div>
         </a>
@@ -27,10 +25,36 @@
       <p class="font-weight-normal">by <a><strong>pelacase.com</strong></a>, 07/08/2018</p>
 
       <a class="btn btn-success" href="https://pelacase.com/blogs/news/7-eco-friendly-bloggers-we-love?gclid=CjwKCAjwkJj6BRA-EiwA0ZVPVoxFGgE9SKGr_oJsNz5Cwej5d2zqa9FHj19RWfaTqTpKtzjA7Pev4BoCrQcQAvD_BwE">Read more</a>
+    </div>
+  </div>
+
+  <div class="row">
+
+    <div class="col-md-4 offset-md-1 mx-3 my-3">
+
+      <div class="view overlay">
+        <img src="../assets/products.jpg" class="img-fluid" >
+        <a>
+          <div class="mask rgba-white-slight"></div>
+        </a>
+      </div>
 
     </div>
 
-  </div>
+    <div class="col-md-7 text-md-left ml-3 mt-3">
 
+      <a href="#!" class="green-text">
+        <h6 class="h6 pb-1"><i class="fas fa-desktop pr-1"></i> Nature</h6>
+      </a>
+
+      <h4 class="h4 mb-4">What are the Benefits of Compostable Packaging?</h4>
+
+      <p class="font-weight-normal">THE BENEFITS OF COMPOSTABLE PACKAGING AND DISPOSABLES
+      You’ve probably heard the term ‘compostable food packaging’, but do you know what it is and how using it could benefit your business? </p>
+      <p class="font-weight-normal">by <a><strong>Nisbets Plc. Catering Equipment Supplies</strong></a>, 07/08/2018</p>
+
+      <a class="btn btn-success" href="https://www.nisbets.co.uk/what-benefits-compostable-packaging-disposables">Read more</a>
+    </div>
+  </div>
 </div>
 </template>
