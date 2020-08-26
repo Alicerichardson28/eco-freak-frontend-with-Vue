04215da1cd11b11ce9a078e4fe64a46690577f77
Parada <paradafaii28@gmail.com>
2020-08-25 22:07:53 -0600

    add content to the cards

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -8,102 +8,135 @@
         <div>
             <h1 class="first-content">Eco-Friendly Products That'll Get <br> You A Little Closer To Saving The Planet</h1>
         </div>
+       
         <div class="cards-container">
             <ul class="cards-ul">
                 <li class="card">
                     <img class="product-image" src="../assets/product-plates1.png">
                     <div class="product-title-container">
                         <h2 class="product-title"> Compostable Plates </h2>
+                        <p class="product-description">These are plates that are made from materials that will degrade quickly and are suitable for composting. </p>
+                        <button class="btn">Read more</button>
                     </div>
                 </li>
                 <li class="card">
                     <img class="product-image" src="../assets/product-dinner-napkin.png">
                     <div class="product-title-container">
                         <h2 class="product-title"> Compostable Bamboo Napkins </h2>
+                        <p class="product-description">compostable napkins have the look and feel of premium cloth napkins. Made from 100% bamboo.</p>
+                        <button class="btn">Read more</button>
                     </div>
                 </li>
                 <li class="card">
                     <img class="product-image" src="../assets/product-bowls.jpg">
                     <div class="product-title-container">
                         <h2 class="product-title"> Compostable Bowls </h2>
+                        <p class="product-description">These products are available in a variety of materials, including palm leaves, sugarcane, bamboo, and molded fiber. </p>
+                        <button class="btn">Read more</button>
                     </div>
                 </li>
                 <li class="card">
                     <img class="product-image" src="../assets/product-hot-cup.jpeg">
                     <div class="product-title-container">
                         <h2 class="product-title"> Compostable Hot Cups </h2>
+                        <p class="product-description">Turn Your Drink Into An Environmental Statement by use a paper cup made entirely from plant starch. </p>
+                        <button class="btn">Read more</button>
                     </div>
                 </li>
                 <li class="card">
                     <img class="product-image" src="../assets/product-takeout-container.jpg">
                     <div class="product-title-container">
                         <h2 class="product-title"> Compostable Takeout Containers </h2>
+                        <p class="product-description">Compostable Takeout Containers Many cities are implementing disposable foam food container bans. </p>
+                        <button class="btn">Read more</button>
                     </div>
                 </li>
                 <li class="card">
                     <img class="product-image" src="../assets/product-tablecloth.jpeg">
                     <div class="product-title-container">
                         <h2 class="product-title"> Compostable Tablecloth </h2>
+                        <p class="product-description">These are plates that are made from materials that will degrade quickly and are suitable for composting. </p>
+                        <button class="btn">Read more</button>
                     </div>
                 </li>
                 <li class="card">
                     <img class="product-image" src="../assets/product-cutleries.png">
                     <div class="product-title-container">
                         <h2 class="product-title"> Compostable Cutleries </h2>
+                        <p class="product-description">These are plates that are made from materials that will degrade quickly and are suitable for composting. </p>
+                        <button class="btn">Read more</button>
                     </div>
                 </li>
                 <li class="card">
                     <img class="product-image" src="../assets/product-cold-cup.jpeg">
                     <div class="product-title-container">
                         <h2 class="product-title"> Compostable PLA Cold Cups </h2>
+                        <p class="product-description">These are plates that are made from materials that will degrade quickly and are suitable for composting. </p>
+                        <button class="btn">Read more</button>
                     </div>
                 </li>
                 <li class="card">
                     <img class="product-image" src="../assets/prosuct-trash-bag1.jpeg">
                     <div class="product-title-container">
                         <h2 class="product-title"> Compostable Trash Bag </h2>
+                        <p class="product-description">These are plates that are made from materials that will degrade quickly and are suitable for composting. </p>
+                        <button class="btn">Read more</button>
                     </div>
                 </li>
                 <li class="card">
                     <img class="product-image" src="../assets/product-straws.png">
                     <div class="product-title-container">
                         <h2 class="product-title"> Bamboo straws </h2>
+                        <p class="product-description">These are plates that are made from materials that will degrade quickly and are suitable for composting. </p>
+                        <button class="btn">Read more</button>
                     </div>
                 </li>
                 <li class="card">
                     <img class="product-image" src="../assets/paper-straws.jpeg">
                     <div class="product-title-container">
                         <h2 class="product-title"> Paper straws </h2>
+                        <p class="product-description">These are plates that are made from materials that will degrade quickly and are suitable for composting. </p>
+                        <button class="btn">Read more</button>
                     </div>
                 </li>
                 <li class="card">
                     <img class="product-image" src="../assets/product-pasta-straws.png">
                     <div class="product-title-container">
                         <h2 class="product-title"> Pasta straws </h2>
+                        <p class="product-description">These are plates that are made from materials that will degrade quickly and are suitable for composting. </p>
+                        <button class="btn">Read more</button>
                     </div>
                 </li>
                 <li class="card">
                     <img class="product-image" src="../assets/product-cotton-buds.jpeg">
                     <div class="product-title-container">
                         <h2 class="product-title"> Bamboo Cotton Buds </h2>
+                        <p class="product-description">These are plates that are made from materials that will degrade quickly and are suitable for composting. </p>
+                        <button class="btn">Read more</button>
                     </div>
                 </li>
                 <li class="card">
                     <img class="product-image" src="../assets/product-toothbrush.jpeg">
                     <div class="product-title-container">
                         <h2 class="product-title"> Bamboo toothbrush </h2>
+                        <p class="product-description">These are plates that are made from materials that will degrade quickly and are suitable for composting. </p>
+                        <button class="btn">Read more</button>
                     </div>
                 </li>
                 <li class="card">
                     <img class="product-image" src="../assets/product-toilet-paper.jpeg">
                     <div class="product-title-container">
                         <h2 class="product-title"> Tree - Free Bath Tissue </h2>
+                        <p class="product-description">These are plates that are made from materials that will degrade quickly and are suitable for composting. </p>
+                        <button class="btn">Read more</button>
                     </div>
                 </li>
                 <li class="card">
                     <img class="product-image" src="../assets/product-market-bag.jpeg">
                     <div class="product-title-container">
                         <h2 class="product-title"> market bag </h2>
+                        <p class="product-description">These are plates that are made from materials that will degrade quickly and are suitable for composting. </p>
+                        <button class="btn">Read more</button>
                     </div>
                 </li>
             </ul>
@@ -191,30 +224,33 @@
     }
 
     .cards-container {
+        margin-top: 15em;
         width: 100%;
-        margin-bottom: 5em;
+        margin-bottom: 3em;
     }
     .cards-ul {
         width: 100%;
         display: grid;
         grid-template-columns: repeat(4, auto);
-        grid-gap: 10px;
+        grid-gap: 130px;
     }
     .card {
         width: 250px;
         height: 300px;
         justify-content: flex-end;
         margin: 0 auto;
-        border-radius: 25px;
     }
     .product-image {
         width: 100%;
         height: 90%;
-        border-radius: 25px;
     }
     .product-title {
         margin-top: 10px;
         margin-bottom: 10px;
     }
+    .product-description{
+        font-size: 80%;
+    }
+
 </style>
 
