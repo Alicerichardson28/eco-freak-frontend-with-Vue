5a1fc9f263e18be7a1f7900d2385b4a10602553b
Parada <paradafaii28@gmail.com>
2020-08-28 18:36:51 -0600

    style header blog post

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,5 +1,12 @@
 <template>
     <header>
+        <div class="center">
+            <div class="title">
+                <h3 class="top">safe environment with</h3>
+                <h1>Eco-Freak</h1>
+                <h3 class="bottom">world happy by your hand</h3>
+            </div>
+        </div>
         <div class="right">
             <label for="search-bar">
                 <img src="../assets/search-icon.png" class="icon" />
@@ -7,68 +14,38 @@
             </label>
         </div>
     </header>
-    <!-- <div class="jumbotron text-center hoverable p-4">
-    <div class="row">
-        <div class="col-md-4 offset-md-1 mx-3 my-3">
-
-        <div class="view overlay">
-            <img src="../assets/eco-friendly.jpg" class="img-fluid" >
-            <a>
-            <div class="mask rgba-white-slight"></div>
-            </a>
-        </div>
-
-        </div>
-
-        <div class="col-md-7 text-md-left ml-3 mt-3">
-
-        <a href="#!" class="green-text">
-            <h6 class="h6 pb-1"><i class="fas fa-desktop pr-1"></i> Nature</h6>
-        </a>
-
-        <h4 class="h4 mb-4">7 Eco Friendly Bloggers we LOVE!</h4>
-
-        <p class="font-weight-normal">If you're interested in living a more eco-friendly life, we've rounded up 10 blogs that you should be reading! 
-        These blogs are encouraging, helpful, and truly break eco-friendly living down in a friendly and approachable way. </p>
-        <p class="font-weight-normal">by <a><strong>pelacase.com</strong></a>, 07/08/2018</p>
-
-        <a class="btn btn-success" href="https://pelacase.com/blogs/news/7-eco-friendly-bloggers-we-love?gclid=CjwKCAjwkJj6BRA-EiwA0ZVPVoxFGgE9SKGr_oJsNz5Cwej5d2zqa9FHj19RWfaTqTpKtzjA7Pev4BoCrQcQAvD_BwE">Read more</a>
-        </div>
-    </div>
-    <br><br><br><br><br>
-    <div class="row">
-
-        <div class="col-md-4 offset-md-1 mx-3 my-3">
-
-        <div class="view overlay">
-            <img src="../assets/products.jpg" class="img-fluid" >
-            <a>
-            <div class="mask rgba-white-slight"></div>
-            </a>
-        </div>
-
-        </div>
-
-        <div class="col-md-7 text-md-left ml-3 mt-3">
-x
-        <a href="#!" class="green-text">
-            <h6 class="h6 pb-1"><i class="fas fa-desktop pr-1"></i> Nature</h6>
-        </a>
-
-        <h4 class="h4 mb-4">What are the Benefits of Compostable Packaging?</h4>
-
-        <p class="font-weight-normal">THE BENEFITS OF COMPOSTABLE PACKAGING AND DISPOSABLES
-        You’ve probably heard the term ‘compostable food packaging’, but do you know what it is and how using it could benefit your business? </p>
-        <p class="font-weight-normal">by <a><strong>Nisbets Plc. Catering Equipment Supplies</strong></a>, 07/08/2018</p>
-
-        <a class="btn btn-success" href="https://www.nisbets.co.uk/what-benefits-compostable-packaging-disposables">Read more</a>
-        </div>
-    </div>
-    </div> -->
 </template>
 
 <style lang="scss">
     header {
+        display: flex;
+        justify-content: center;
+        padding: 50px 50px 25px;
+        margin-left: 40em;
+
+        .center {
+            h3 {
+                font-size: 20px;
+                font-style: italic;
+                font-weight: 400;
+                margin: 0;
+
+                &.bottom {
+                    text-align: right;
+                }
+                &.top {
+                    text-align: left;
+                }
+            }
+
+            h1 {
+                color: #171717;
+                font-size: 72px;
+                margin: 0;
+            }
+
+        }
+
         .right {
            flex: 1; 
            display: flex;
