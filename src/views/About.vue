ad57b0950e18d75f76b22601ffd0f42d433692e4
Parada <paradafaii28@gmail.com>
2020-09-09 08:31:53 -0600

    style about pages

--- src/views/About.vue
+++ src/views/About.vue
@@ -17,6 +17,13 @@
       </p>
       <br>
       <hr>
+      <br>
+      <h2>50 Ways to Help the Planet - <br> Save Our Environment and Planet Earth</h2>
+      <br>
+      <p>According from <a href="https://www.50waystohelp.com/"> 50 way to help.com </a> this website is show you how you can help to save this world. <br>
+      When you heard the world saveing the planet soundes big doesn't it? But this website going to show you <br>  
+      how to save this world in the simple ways to do and can make this world a bit better everyday. 
+      </p>
   </div>
 </template>
 
